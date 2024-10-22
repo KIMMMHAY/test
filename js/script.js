5e88dfc5ae6ab25bff27b39eed2925dfcd0e11a9
KIMMMHAY <128764475+KIMMMHAY@users.noreply.github.com>
2024-10-22 23:28:31 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // 여기다가 코드를 작성하세요
+});
+
+//.button이라는 clss를 가진 태그를 클릭하면, (조건)
+//.box라는 clss를 가진 div의 css 속성중
+//background-color를 blue로 바꾸고 싶어
+
+//모든 css컨트롤 가능
+//line-height, letter-spacing, 인덴트, margin, padding
+
+//네모를 파란색으로 바꾸고 싶어
+// .box라는 class를 가진 div를 파란색으로 바꾸고 싶어
+// .box라는 class를 가진 div의 background-color를 blue로 바꾸고 싶어
+// .box라는 class를 가진 div의 css 속성 중, background-color를 blue로 바꾸고 싶어.
+
+// 노란색 - method
